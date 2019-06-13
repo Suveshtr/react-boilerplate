c10bebc0e8a48b40eed5e2151336b18fdd0120aa
Suveshtr <suvesh.trivedi@gmail.com>
2019-06-12 21:41:39 -0400

    added testing globals

--- src/app.spec.js
+++ src/app.spec.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 //add some extra matchers that are specific to testing DOM
-import 'jest-dom/extend-expect'
+//moved to tstSetup.js
+//import 'jest-dom/extend-expect'
 //run some code after each test that'll clean up the virtual DOM to make sure that we don't 
 //have any state hanging around from one test to the next
-import '@testing-library/react/cleanup-after-each'
+//moved to tstSetup.js
+//import '@testing-library/react/cleanup-after-each' 
 import App from './App'
 
 describe('App', () => {
